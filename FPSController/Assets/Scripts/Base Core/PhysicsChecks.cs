051e62a986042152a2eacf0505bebe81e27afb44
Akshay Bharadhwaj <46255105+JackdawSama@users.noreply.github.com>
2023-07-14 01:47:46 -0400

    Updated MouseLook Script and added PhysicsCheck Script

---
+++ FPSController/Assets/Scripts/Base Core/PhysicsChecks.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsChecks : MonoBehaviour
+{
+    //Write Box Cast for Ground Check
+    //Write Raycast for Step and Slope Check
+    //Write Sphere Cast for Wall Check
+}
--- FPSController/Assets/Scripts/Base Core/PlayerLook.cs
+++ FPSController/Assets/Scripts/Base Core/PlayerLook.cs
@@ -10,6 +10,7 @@ public class PlayerLook : MonoBehaviour
 
     [Header("Mouse Look Settings")]
     [SerializeField] private float mouseSensitivity = 3.5f;
+    
     [SerializeField] [Range(0.0f, 90.0f)] private float verticalLookLimits = 90.0f;
 
     [Header("Mouse Cursor Settings")]
@@ -18,6 +19,7 @@ public class PlayerLook : MonoBehaviour
     //MouseLook Input Variables
     Vector2 sens;
     float xRot = 0f;
+    float yRot = 0f;
 
 
     void Start()
@@ -40,6 +42,8 @@ public class PlayerLook : MonoBehaviour
         sens.x = Input.GetAxisRaw("Mouse X") * mouseSensitivity * Time.deltaTime;
         sens.y = Input.GetAxisRaw("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
+        yRot += sens.x;
+
         //Clamp Vertical Rotation
         xRot -= sens.y;
         xRot = Mathf.Clamp(xRot, -verticalLookLimits, verticalLookLimits);
@@ -51,7 +55,8 @@ public class PlayerLook : MonoBehaviour
     public void HandleMouseLook()
     {
         //Player Body Rotation Horizontally
-        playerBody.Rotate(Vector3.up * sens.x);
+        //playerBody.Rotate(Vector3.up * sens.x);
+        playerBody.transform.rotation = Quaternion.Euler(0f, yRot, 0f);
         
         //Function to update camera vertical look rotation
         playerCamera.transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
