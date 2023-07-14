3a2b4ae0292f64936c4ade51e67db4f468f6e289
Akshay Bharadhwaj <46255105+JackdawSama@users.noreply.github.com>
2023-07-14 01:55:52 -0400

    Scene Updates

--- FPSController/Assets/Scripts/Base Core/PlayerController.cs
+++ FPSController/Assets/Scripts/Base Core/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 //Think what the PlayerController does. This has to integrate with Player Look, Player Movement and Player Health.
 [RequireComponent(typeof(PlayerLook))]
 [RequireComponent(typeof(PlayerMovement))]
+[RequireComponent(typeof(PhysicsChecks))]
 
 public class PlayerController : MonoBehaviour
 {
