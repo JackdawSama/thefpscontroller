ec30efd85be89038419be75adf9d5ac035ff8dc8
Akshay Bharadhwaj <46255105+JackdawSama@users.noreply.github.com>
2023-07-15 00:20:58 -0400

    Ground Check

--- FPSController/Assets/Scripts/Base Core/PhysicsChecks.cs
+++ FPSController/Assets/Scripts/Base Core/PhysicsChecks.cs
@@ -7,4 +7,39 @@ public class PhysicsChecks : MonoBehaviour
     //Write Box Cast for Ground Check
     //Write Raycast for Step and Slope Check
     //Write Sphere Cast for Wall Check
+
+    [Header("Ground Check Variables")]
+    [SerializeField] float GroundCheckDistance = 1f;
+    [SerializeField] private LayerMask groundMask = 0;
+    [SerializeField] private Vector3 boxSize = Vector3.zero;
+
+    [Header("Step/Slope Check Variables")]
+    [SerializeField] float StepCheckDistance = 1f;
+
+    [Header("Wall Check Variables")]
+    [SerializeField] float WallCheckDistance = 1f;
+
+    void Update()
+    {
+        if(GroundCheck())
+        {
+            Debug.Log("Grounded");
+        }
+        else
+        {
+            Debug.Log("Not Grounded");
+        }
+    }
+
+    bool GroundCheck()
+    {
+        if(Physics.BoxCast(transform.position, boxSize, - Vector3.up, transform.rotation, GroundCheckDistance, groundMask))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 }
